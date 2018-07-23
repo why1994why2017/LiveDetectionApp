1cc7d39d82d529fe0230caf69301b240ee2b3b5b
Iori wang <wanghaoyu01628@126.com>
2018-07-23 13:27:31 +0800

    移植文件

---
+++ app/src/main/java/com/example/wang/livedetectionapp/mode/MenuInfo.java
@@ -0,0 +1,57 @@
+package com.example.wang.livedetectionapp.mode;
+
+public class MenuInfo {
+    private String img;
+    private String name;
+    private String description;
+    private String price;
+    private int count;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getImage() {
+        return img;
+    }
+
+    public void setImage(String image) {
+        this.img = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
